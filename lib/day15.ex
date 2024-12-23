4dd62d1002e9dc6165f0ebcaa3db9d726416e17c
rertyy <107781565+rertyy@users.noreply.github.com>
2024-12-23 21:59:09 +0800

    Remove day 15 part2 (not working)

--- lib/day15.ex
+++ lib/day15.ex
@@ -114,101 +114,101 @@ defmodule Day15 do
     end)
   end
 
-  def part2(filename) do
-    input = File.read!(filename)
-    {grid, nrows, ncols, moves} = parse(input)
-    grid = resize_grid(grid)
-    ncols = ncols * 2
-    AocUtil.inspect_grid(grid, nrows, ncols)
+  # def part2(filename) do
+  #   input = File.read!(filename)
+  #   {grid, nrows, ncols, moves} = parse(input)
+  #   grid = resize_grid(grid)
+  #   ncols = ncols * 2
+  #   AocUtil.inspect_grid(grid, nrows, ncols)
+  #
+  #   start = find_robot(grid)
+  #   final_grid = move_robot2(grid, nrows, ncols, moves, start)
+  #   calc_grid(final_grid, @box_left)
+  # end
 
-    start = find_robot(grid)
-    final_grid = move_robot2(grid, nrows, ncols, moves, start)
-    calc_grid(final_grid, @box_left)
-  end
+  ## robot moves to empty
+  ## robot fails due to at least one box next to wall
+  ## robot fails due to next to wall
+  ## robot pushes box(es) to empty
+  # defp move_robot2(grid, nrows, ncols, moves, robot) do
+  #   fup = fn i, j, dc -> {i - dc, j} end
+  #   fdown = fn i, j, dc -> {i + dc, j} end
+  #   fleft = fn i, j, dc -> {i, j - dc} end
+  #   fright = fn i, j, dc -> {i, j + dc} end
+  #
+  #   {final_grid, _robot} =
+  #     Enum.reduce(moves, {grid, robot}, fn move, {grid, robot} ->
+  #       # iterator, dir to move in, opposite dir 
+  #       {iter, get_new} =
+  #         case move do
+  #           @up -> {1..nrows, fup}
+  #           @down -> {1..nrows, fdown}
+  #           @left -> {1..ncols, fleft}
+  #           @right -> {1..ncols, fright}
+  #         end
+  #
+  #       check_move(grid, robot, iter, get_new)
+  #     end)
+  #
+  #   final_grid
+  # end
 
-  # robot moves to empty
-  # robot fails due to at least one box next to wall
-  # robot fails due to next to wall
-  # robot pushes box(es) to empty
-  defp move_robot2(grid, nrows, ncols, moves, robot) do
-    fup = fn i, j, dc -> {i - dc, j} end
-    fdown = fn i, j, dc -> {i + dc, j} end
-    fleft = fn i, j, dc -> {i, j - dc} end
-    fright = fn i, j, dc -> {i, j + dc} end
+  # defp check_move(grid, robot, iter, get_new) do
+  #   {i, j} = robot
+  #   {num_boxes, coord, dist} = get_space(grid, robot, iter, get_new)
+  #
+  #   cond do
+  #     # no need to move any boxes, or robot next to wall
+  #     dist == num_boxes * 2 ->
+  #       {grid, robot}
+  #
+  #     num_boxes == 0 ->
+  #       # move robot 1 square
+  #       new_robot = get_new.(i, j, 1)
+  #
+  #       grid = %{grid | robot => @empty}
+  #       grid = %{grid | new_robot => @robot}
+  #       {grid, new_robot}
+  #
+  #     true ->
+  #       {grid, robot} =
+  #         Enum.reduce(1..num_boxes, {grid, robot, @box_right}, fn dc, acc ->
+  #           {grid, coord, last_box_side} = acc
+  #           last_box_side = (last_box_side == @box_left && @box_right) || @box_left
+  #
+  #           new_coord = get_new.(i, j, 1 + dc)
+  #
+  #           {grid, new_coord, last_box_side}
+  #         end)
+  #
+  #       {grid, robot}
+  #   end
+  # end
 
-    {final_grid, _robot} =
-      Enum.reduce(moves, {grid, robot}, fn move, {grid, robot} ->
-        # iterator, dir to move in, opposite dir 
-        {iter, get_new} =
-          case move do
-            @up -> {1..nrows, fup}
-            @down -> {1..nrows, fdown}
-            @left -> {1..ncols, fleft}
-            @right -> {1..ncols, fright}
-          end
-
-        check_move(grid, robot, iter, get_new)
-      end)
-
-    final_grid
-  end
-
-  defp check_move(grid, robot, iter, get_new) do
-    {i, j} = robot
-    {num_boxes, coord, dist} = get_space(grid, robot, iter, get_new)
-
-    cond do
-      # no need to move any boxes, or robot next to wall
-      dist == num_boxes * 2 ->
-        {grid, robot}
-
-      num_boxes == 0 ->
-        # move robot 1 square
-        new_robot = get_new.(i, j, 1)
-
-        grid = %{grid | robot => @empty}
-        grid = %{grid | new_robot => @robot}
-        {grid, new_robot}
-
-      true ->
-        {grid, robot} =
-          Enum.reduce(1..num_boxes, {grid, robot, @box_right}, fn dc, acc ->
-            {grid, coord, last_box_side} = acc
-            last_box_side = (last_box_side == @box_left && @box_right) || @box_left
-
-            new_coord = get_new.(i, j, 1 + dc)
-
-            {grid, new_coord, last_box_side}
-          end)
-
-        {grid, robot}
-    end
-  end
-
-  defp resize_grid(grid) do
-    grid
-    |> Enum.reduce(%{}, fn {{i, j}, val}, acc ->
-      case val do
-        @wall ->
-          acc = Map.put(acc, {i, 2 * j}, @wall)
-          acc = Map.put(acc, {i, 2 * j + 1}, @wall)
-          acc
-
-        @empty ->
-          acc = Map.put(acc, {i, 2 * j}, @empty)
-          acc = Map.put(acc, {i, 2 * j + 1}, @empty)
-          acc
-
-        @box ->
-          acc = Map.put(acc, {i, 2 * j}, @box_left)
-          acc = Map.put(acc, {i, 2 * j + 1}, @box_right)
-          acc
-
-        @robot ->
-          acc = Map.put(acc, {i, 2 * j}, @robot)
-          acc = Map.put(acc, {i, 2 * j + 1}, @empty)
-          acc
-      end
-    end)
-  end
+  # defp resize_grid(grid) do
+  #   grid
+  #   |> Enum.reduce(%{}, fn {{i, j}, val}, acc ->
+  #     case val do
+  #       @wall ->
+  #         acc = Map.put(acc, {i, 2 * j}, @wall)
+  #         acc = Map.put(acc, {i, 2 * j + 1}, @wall)
+  #         acc
+  #
+  #       @empty ->
+  #         acc = Map.put(acc, {i, 2 * j}, @empty)
+  #         acc = Map.put(acc, {i, 2 * j + 1}, @empty)
+  #         acc
+  #
+  #       @box ->
+  #         acc = Map.put(acc, {i, 2 * j}, @box_left)
+  #         acc = Map.put(acc, {i, 2 * j + 1}, @box_right)
+  #         acc
+  #
+  #       @robot ->
+  #         acc = Map.put(acc, {i, 2 * j}, @robot)
+  #         acc = Map.put(acc, {i, 2 * j + 1}, @empty)
+  #         acc
+  #     end
+  #   end)
+  # end
 end
